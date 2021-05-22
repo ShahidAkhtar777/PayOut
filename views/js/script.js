e4cd60059dde795870e75949a75ac6726e73b27f
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-05-22 20:59:34 +0530

    Implemented project for random generated data

--- views/js/script.js
+++ views/js/script.js
@@ -1,62 +1,93 @@
-class BinaryHeap{
-    constructor() {
-        this.heap = [];
-    }
+class BinaryHeap {
+ 
+     constructor() {
+         this.heap = [];
+     }
+ 
+     insert(value) {
+         this.heap.push(value);
+         this.bubbleUp();
+     }
+ 
+     size() {
+         return this.heap.length;
+     }
+ 
+     empty(){
+         return ( this.size()===0 );
+     }
 
-    size() {
-        return this.heap.length;
-    }
-
-    empty(){
-        return ( this.size()===0 );
-    }
-
-    insert(value) {
-        console.log(value);
-        this.heap.push(value);
-        this.bubbleUp();
-    }
-
-    bubbleUp() 
-    {
-        let index = this.size() - 1;
-        while (index > 0) 
-        {
-            let element = this.heap[index],
-                parentIndex = Math.floor((index - 1) / 2),
-                parent = this.heap[parentIndex];
-
-            if (parent[0] >= element[0]) break;
-            this.heap[index] = parent;
-            this.heap[parentIndex] = element;
-            index = parentIndex
-        }
-    }
-   
-};
-
+     bubbleUp() {
+         let index = this.size() - 1;
+ 
+         while (index > 0) {
+             let element = this.heap[index],
+                 parentIndex = Math.floor((index - 1) / 2),
+                 parent = this.heap[parentIndex];
+ 
+             if (parent[0] >= element[0]) break;
+             this.heap[index] = parent;
+             this.heap[parentIndex] = element;
+             index = parentIndex
+         }
+     }
+ 
+     extractMax() {
+         const max = this.heap[0];
+         const tmp = this.heap.pop();
+         if(!this.empty()) {
+             this.heap[0] = tmp;
+             this.sinkDown(0);
+         }
+         return max;
+     }
+ 
+     sinkDown(index) {
+ 
+         let left = 2 * index + 1,
+             right = 2 * index + 2,
+             largest = index;
+         const length = this.size();
+ 
+         // console.log(this.heap[left], left, length, this.heap[right], right, length, this.heap[largest]);
+ 
+         if (left < length && this.heap[left][0] > this.heap[largest][0]) {
+             largest = left
+         }
+         if (right < length && this.heap[right][0] > this.heap[largest][0]) {
+             largest = right
+         }
+         // swap
+         if (largest !== index) {
+             let tmp = this.heap[largest];
+             this.heap[largest] = this.heap[index];
+             this.heap[index] = tmp;
+             this.sinkDown(largest)
+         }
+     }
+ }
+ 
 
 onload = function () 
 {
     // create a network
-    var curr_data;
-    var container = document.getElementById('mynetwork');
-    var container2 = document.getElementById('mynetwork2');
-    var genNew = document.getElementById('generate-graph');
-    var solve = document.getElementById('solve');
+    let curr_data,transactions_no;
+    const container = document.getElementById('mynetwork');
+    const container2 = document.getElementById('mynetwork2');
+    const genNew = document.getElementById('generate-graph');
+    const solve = document.getElementById('solve');
+    const transactions = this.document.getElementById('trans').innerText;
 
     // initialise graph options
-    var options = {
+    const options = {
         edges: {
-            arrows: 
-            {
+            arrows: {
                 to: true
             },
             labelHighlightBold: true,
             font: {
                 size: 20
-            },
-            width: 1.3
+            }
         },
         nodes: {
             font: '12px arial red',
@@ -64,70 +95,127 @@ onload = function ()
                 label: true
             },
             shape: 'icon',
-            icon: 
-            {
+            icon: {
                 face: 'FontAwesome',
                 code: '\uf183',
-                size: 50,  //50,
+                size: 50,
                 color: '#991133',
             }
         }
     };
 
-    // initialize network
-    var network = new vis.Network(container);
+    // initialize your network!
+    let network = new vis.Network(container);
     network.setOptions(options);
+    let network2 = new vis.Network(container2);
+    network2.setOptions(options);
 
     function createData()
     {
-        let vertices = Math.floor(Math.random() * 8) + 2;
+        const vertices = Math.floor(Math.random() * 8) + 2;
+
+        // Adding people to nodes array
         let nodes = [];
-        for(i=1;i<=vertices;i++)
-        {
-            nodes.push({id:i, label:"Person "+i})
+        for(let i=1;i<=vertices;i++){
+            nodes.push({id:i, label:"Person " + i})
         }
         nodes = new vis.DataSet(nodes);
 
-        let edges = [];
-        for(i=1;i<=vertices;i++)
-        {
-            for(j=i+1;j<=vertices;j++)
+        // Dynamically creating edges with random amount to be paid from one to another friend
+        const edges = [];
+        for(let i=1;i<=vertices;i++){
+            for(let j=i+1;j<=vertices;j++)
             {
-                if(Math.random() > 0.5) //To make lesser no of edges n(n-1)/4 approx
-                {
-                    if(Math.random() > 0.5) // To make forward and backward edges
+                // Modifies the amount of edges added in the graph around n(n-1)/4 
+                if(Math.random() > 0.5){
+                    // direction of cash flow on edge
+                    if(Math.random() > 0.5)
                         edges.push({from: i, to: j, label: String(Math.floor(Math.random()*100)+1)});
                     else
                         edges.push({from: j, to: i, label: String(Math.floor(Math.random()*100)+1)});
                 }
             }
         }
-        data = {
+        const data = {
             nodes: nodes,
             edges: edges
         };
-        curr_data = data;
+        return data;
     }
 
     genNew.onclick = function () {
-        createData();
-        network.setData(curr_data);
+        const data = createData();
+        curr_data = data;
+        network.setData(data);
         container2.style.display = "none";
+        document.getElementById('trans').innerText = transactions;
     };
 
     solve.onclick = function () {
         container2.style.display = "inline";
-        let heap = new BinaryHeap();
-        console.log(BinaryHeap);
-        for(let i=0;i<curr_data['nodes'].length;i++)
-        {
-            const edge = curr_data['edges'][i];
-            heap.insert([parseInt(edge['label']),parseInt(edge['from'])]);
-        }
-        console.log(heap);
+        const solvedData = solveData();
+        network2.setData(solvedData);
     };
 
-   
-    genNew.click();
-};
+    function solveData() {
+        let data = curr_data;
+        const vertices = data['nodes'].length;
+        const vals = Array(vertices).fill(0);
+        // Calculating net balance of each person
+        for(let i=0;i<data['edges'].length;i++) 
+        {
+            const edge = data['edges'][i];
+            vals[edge['to'] - 1] += parseInt(edge['label']);
+            vals[edge['from'] - 1] -= parseInt(edge['label']);
+        }
 
+        const pos_heap = new BinaryHeap();
+        const neg_heap = new BinaryHeap();
+
+        for(let i=0;i<vertices;i++)
+        {
+            if(vals[i]>0)
+            {
+                pos_heap.insert([vals[i],i]);
+            } else{
+                neg_heap.insert(([-vals[i],i]));
+                vals[i] *= -1;
+            }
+        }
+
+        const new_edges = [];
+        while(!pos_heap.empty() && !neg_heap.empty())
+        {
+            const mx = pos_heap.extractMax();
+            const mn = neg_heap.extractMax();
+
+            const amt = Math.min(mx[0],mn[0]);
+            const to = mx[1];
+            const from = mn[1];
+
+            new_edges.push({from: from+1, to: to+1, label: String(Math.abs(amt))});
+            vals[to] -= amt;
+            vals[from] -= amt;
+
+            if(mx[0] > mn[0])
+            {
+                pos_heap.insert([vals[to],to]);
+            } else if(mx[0] < mn[0]){
+                neg_heap.insert([vals[from],from]);
+            }
+        }
+
+        transactions_no = new_edges.length;
+        document.getElementById('trans').innerText = transactions + ' : ' + transactions_no;
+        
+
+        data = {
+            nodes: data['nodes'],
+            edges: new_edges
+        };
+        return data;
+    }
+
+    genNew.click();
+
+};
